e3428f12b930cd162bcc04f2d894babcb5c56cf5
VEDANTKESARWANI <53945486+VEDANTKESARWANI@users.noreply.github.com>
2026-05-16 23:01:44 +0530

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>q;
+        for(auto x:stones)
+        {
+            q.push(x);
+        }
+        while(q.size()>1)
+        {
+            int first=q.top();
+            q.pop();
+            int second=q.top();
+            q.pop();
+            q.push(first-second);
+
+        }
+        return q.top();
+        
+    }
+};
